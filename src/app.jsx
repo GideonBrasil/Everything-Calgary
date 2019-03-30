0c9f9baa33cca1eaa74cffe7f25d01c1460bf6eb
GideonBrasil <gidxx_z@yahoo.com>
2019-03-30 21:53:24 +0000

    fix app.jsx conflicts

--- src/app.jsx
+++ src/app.jsx
@@ -1,19 +1,6 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 const React = require("react");
 const ReactDOM = require("react-dom");
-=======
-const React = require('react');
-const ReactDOM = require('react-dom');
-import Main from './main.jsx'
->>>>>>> 23090f131c073fc0d78326fba483edfab87a36ad
-=======
-const React = require("react");
-const ReactDOM = require("react-dom");
->>>>>>> feature/google-maps
 
-
-<<<<<<< HEAD
 class HelloMessage extends React.Component {
   render() {
     return (
@@ -28,14 +15,3 @@ ReactDOM.render(
   <HelloMessage name="Darren" />,
   document.getElementById("react-root")
 );
-<<<<<<< HEAD
-=======
-/*
-This is what is rendered. All builds go into main.jsx and are imported here
------------------------------------------------------------------------------
-*/
-
-ReactDOM.render( <Main />,  document.getElementById('root'));
->>>>>>> 23090f131c073fc0d78326fba483edfab87a36ad
-=======
->>>>>>> feature/google-maps
