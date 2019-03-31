2c94120095ac58c1af48371dc66d36f760f57c99
GideonBrasil <gidxx_z@yahoo.com>
2019-03-31 15:50:31 +0000

    fix webpack config for bundle.js and mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,18 +2,17 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    './src/index.jsx'
-  ],
+  mode: 'development',
+  entry: {
+    index: './src/index.jsx'
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js',
     publicPath: '/public/javascripts/'
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
