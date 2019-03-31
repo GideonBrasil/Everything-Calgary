6d242493e69f0aafe10c316e43540924e18e735e
sddanila <sddanila@yahoo.co.uk>
2019-03-31 16:52:45 +0000

    add modules for footer, navbar, jumbotron with bootstrap

---
+++ src/NavBar.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class NavBar extends Component {
+  render() {
+    return (
+      <nav className="navbar sticky-top navbar-light bg-light">
+        <a className="navbar-brand">Navbar</a>
+        <form className="form-inline">
+          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+      </nav>
+    );
+  }
+}
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-const React = require('react');
+import React, {Component} from 'react';
 require("../styles/main.scss");
 import Header from './sections/header.jsx';
 import SearchBar from './sections/search_bar.jsx';
@@ -9,12 +9,14 @@ This is the main structure container all sections will be imported here
 --------------------------------------------------------------------------
 */
 
-class Main extends React.Component {
+class Main extends Component {
   render() {
     return (
-      <div className="main">
-        <Header />
-        <SearchBar />
+      <div className="jumbotron">
+        <h1 className="display-4">Welcome to Calgary!</h1>
+        <p className="lead">This is a simple app to help you keep up with what's going on in our city!</p>
+        <a className="btn btn-primary btn-lg" href="#" role="button">Select Your Neighborhood</a>
+        <a className="btn btn-danger btn-lg" href="#" role="button">Calgary Now</a>
       </div>
     );
   }
