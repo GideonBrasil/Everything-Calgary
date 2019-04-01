163ee227c1de1d85b4e14be875646540c884d194
sddanila <sddanila@yahoo.co.uk>
2019-04-01 22:39:24 +0000

    add autocomplete functionality to navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import Autocomplete from "./Autocomplete.jsx";
+import suggestions from "../helper/communities.js"
 
 
 class NavBar extends Component {
@@ -8,7 +11,8 @@ class NavBar extends Component {
       <nav className="navbar sticky-top navbar-light">
         <a className="navbar-brand"><img id="logo" src="/public/images/logo1.png" alt="logo"></img> Everything Calgary</a>
         <form className="form-inline">
-          <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+          <Autocomplete className="form-control mr-sm-2" type="search" placeholder="Search" suggestions={suggestions}/>
+          {/* <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input> */}
           <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
         </form>
       </nav>
