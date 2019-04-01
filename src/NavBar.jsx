5d8eec7bbfa9ed68d1619a8857bf6d6245a5d6c5
sddanila <sddanila@yahoo.co.uk>
2019-04-01 18:54:09 +0000

    add background image to jumbotron and logo to navbar as well as fix footer to bottom

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,10 +1,12 @@
 import React, {Component} from 'react';
 
+
 class NavBar extends Component {
+
   render() {
     return (
       <nav className="navbar sticky-top navbar-light">
-        <a className="navbar-brand"><img src="/public/images/logo1.png" id="logo"> </img>Everything Calgary</a>
+        <a className="navbar-brand"><img id="logo" src="/public/images/logo1.png" alt="logo"></img> Everything Calgary</a>
         <form className="form-inline">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
           <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
