81de5c04c686491235e7ce87c5c88d7b64129379
sddanila <sddanila@yahoo.co.uk>
2019-04-01 21:55:29 +0000

    add feature to switch between jumbotron and map/sidenav once button is clicked

--- src/SideNav.jsx
+++ src/SideNav.jsx
@@ -3,8 +3,23 @@ import React, {Component} from 'react';
 class Sidenav extends Component {
   render() {
     return (
-      <div>
-      </div>
+      <nav id="sidebar">
+            <div className="sidebar-header">
+                <h3>Show on map...</h3>
+            </div>
+
+            <ul className="list-unstyled components">
+                <li className="active">
+                    <a href="#">Crime</a>
+                </li>
+                <li>
+                    <a href="#">Traffic incidents</a>
+                </li>
+                <li>
+                    <a href="#">Something else</a>
+                </li>
+            </ul>
+        </nav>
     );
   }
 }
--- src/app.jsx
+++ src/app.jsx
@@ -5,11 +5,27 @@ import Footer from "./Footer.jsx";
 import GoogleMaps from "./GoogleMaps.jsx";
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = ({
+      jumbotron: true,
+    });
+    
+    this.handleClick= this.handleClick.bind(this);
+  }
+
+  handleClick() {
+    console.log('clicked')
+    this.setState(state => ({
+      jumbotron: false,
+    }));
+  }
+
   render() {
     return (
       <div>
         <NavBar />
-        <Main />
+        <Main click={this.handleClick} jumbotron={this.state.jumbotron}/>
         <GoogleMaps />
         <Footer />
       </div>
