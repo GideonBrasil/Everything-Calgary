2328f824ac84a600dbef7e1904128330f7bc6a96
sddanila <sddanila@yahoo.co.uk>
2019-04-01 17:08:05 +0000

    add logo file to project and to navbar

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -3,11 +3,11 @@ import React, {Component} from 'react';
 class NavBar extends Component {
   render() {
     return (
-      <nav className="navbar sticky-top navbar-light bg-light">
-        <a className="navbar-brand">Everything Calgary</a>
+      <nav className="navbar sticky-top navbar-light">
+        <a className="navbar-brand"><img src="/public/images/logo1.png" id="logo"> </img>Everything Calgary</a>
         <form className="form-inline">
           <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          <button className="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
         </form>
       </nav>
     );
