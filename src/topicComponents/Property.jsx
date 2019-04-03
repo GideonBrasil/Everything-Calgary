6c902518dc377353178f2e104e9ca989c70b8c7a
sddanila <sddanila@yahoo.co.uk>
2019-04-03 15:37:50 +0000

    add hardcoded data to property module

--- src/topicComponents/Property.jsx
+++ src/topicComponents/Property.jsx
@@ -2,8 +2,33 @@ import React, { Component } from "react";
 
 class Property extends Component {
   render() {
+    const tableData = this.props.propertyData.range.map(data => {
+      return (
+        <tr>
+          <td>{data.community_name}</td>
+          <td>{data.median_assessed_value}</td>
+          <td>{data.number_of_taxable_accounts}</td>
+        </tr>
+      )
+
+    })
     return (
-      <h1>This is the Property Data</h1>
+      <React.Fragment>
+        <h1>This is the Property Data</h1>
+        <h3>Your property is ranked ${this.props.propertyData.targetIndex} out of 195 communities.</h3>
+        <h6>The highest median property value in Calgary is ${this.props.propertyData.highestValue}</h6>
+        <h6>The lowest median property value in Calgary is ${this.props.propertyData.lowestValue}</h6>
+        <table>
+            <thead>
+              <tr>
+                <th>Community Name</th>
+                <th>Median Assessed Property Value</th>
+                <th>Number of Taxable Accounts</th>
+              </tr>
+            </thead>
+            {/* <td>{tableData}</td> */}
+        </table>
+      </React.Fragment>
     );
   }
 }
