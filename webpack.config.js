403e24131631e57d170ce723bc76272df232b15d
Darren Picard <darrenpicard25@gmail.com>
2019-04-04 20:12:31 +0000

    Set up public directory
    moved files into public and set up builder function so files can be added to server very easily

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     index: "./src/index.jsx"
   },
   output: {
-    path: path.join(__dirname, "dist"),
+    path: path.join(__dirname, "./public/build"),
     filename: "bundle.js",
     publicPath: "/build/"
   },
