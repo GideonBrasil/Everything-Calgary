85e888274e5509db7d2cfedfcf489495df0f199b
vagrant <tp.bowes@gmail.com>
2019-04-04 23:33:17 +0000

    address gitting issues, closing branch

--- src/topicComponents/Garbage.jsx
+++ src/topicComponents/Garbage.jsx
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import { Modal, CardDeck, Card } from "react-bootstrap";
 
 class Garbage extends Component {
-
   constructor(props) {
     super(props);
     this.state = {
