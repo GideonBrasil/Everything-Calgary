8e8ed5cf7810be0492ba6a65303ab54c3f659f6a
GideonBrasil <gidxx_z@yahoo.com>
2019-04-07 21:32:28 +0000

    clean up code

--- src/topicComponents/Crime.jsx
+++ src/topicComponents/Crime.jsx
@@ -10,7 +10,7 @@ class Crime extends Component {
   }
 
   handleTabClick(link) {
-    this.setState({link})
+    this.setState({ link });
   }
 
   componentDidMount() {
@@ -20,7 +20,7 @@ class Crime extends Component {
         console.log(data);
         this.setState(state => ({
           crimeStats: data
-        }))
+        }));
       })
       .catch(err => {
         console.log(err);
@@ -31,40 +31,46 @@ class Crime extends Component {
     const { crimeStats, link } = this.state;
     let activeCrimeStats;
     if (crimeStats) {
-      activeCrimeStats = link == '#lst12Mos' ? crimeStats.yearCrimeStats : crimeStats.monthCrimeStats
+      activeCrimeStats =
+        link == "#lst12Mos"
+          ? crimeStats.yearCrimeStats
+          : crimeStats.monthCrimeStats;
     }
-    // if (!Object.keys(crimeStats).length) return null;
+
     return (
       <Modal
-      show
-      onHide={() => this.props.changeTopic("")}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      dialogClassName="modal-60w"
+        show
+        onHide={() => this.props.changeTopic("")}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        dialogClassName="modal-60w"
       >
-      <Modal.Header closeButton>
-        <Modal.Title>Crime Statistics</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        {!crimeStats ? <h4>Loading...</h4> : (
-        <Card>
-          <Card.Header>
-            <Nav
-              justify variant="tabs"
-              defaultActiveKey="#lstMos"
-              onSelect={selectedKey => this.handleTabClick(selectedKey)}
-              >
-              <Nav.Item>
-                <Nav.Link href="#lstMos">Last Month</Nav.Link>
-              </Nav.Item>
-              <Nav.Item>
-                <Nav.Link href="#lst12Mos">Last 12 Months</Nav.Link>
-              </Nav.Item>
-            </Nav>
-          </Card.Header>
-          <Card.Body>
-            <Card.Title>For the month of Feb</Card.Title>
+        <Modal.Header closeButton>
+          <Modal.Title>Crime Statistics</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {!crimeStats ? (
+            <h4>Loading...</h4>
+          ) : (
+            <Card>
+              <Card.Header>
+                <Nav
+                  justify
+                  variant="tabs"
+                  defaultActiveKey="#lstMos"
+                  onSelect={selectedKey => this.handleTabClick(selectedKey)}
+                >
+                  <Nav.Item>
+                    <Nav.Link href="#lstMos">Last Month</Nav.Link>
+                  </Nav.Item>
+                  <Nav.Item>
+                    <Nav.Link href="#lst12Mos">Last 12 Months</Nav.Link>
+                  </Nav.Item>
+                </Nav>
+              </Card.Header>
+              <Card.Body>
+                <Card.Title>For the month of Feb</Card.Title>
                 <Table striped bordered hover>
                   <thead>
                     <tr>
@@ -74,24 +80,52 @@ class Crime extends Component {
                     </tr>
                   </thead>
                   <tbody>
-                      {activeCrimeStats.map(deets => (
-                        <React.Fragment key={deets.category}>
-                          <tr>
-                            <td>{deets.category}</td>
-                            <td>{Number(deets.commNum).toLocaleString()}</td>
-                            <td>{Number(deets.yycNum).toLocaleString()}</td>
-                          </tr>
-                        </React.Fragment> 
-                        ))}
-                          <tr>
-                            <td><strong>TOTAL</strong></td>
-                            <td><strong>{activeCrimeStats = link == '#lst12Mos' ? Number(crimeStats.totalCommCrime12).toLocaleString() : Number(crimeStats.totalCommCrimeMonth).toLocaleString()}</strong></td>
-                            <td><strong>{activeCrimeStats = link == '#lst12Mos' ? Number(crimeStats.totalYYCCrime12).toLocaleString() : Number(crimeStats.totalYYCCrimeMonth).toLocaleString()}</strong></td>
-                          </tr>
+                    {activeCrimeStats.map(deets => (
+                      <React.Fragment key={deets.category}>
+                        <tr>
+                          <td>{deets.category}</td>
+                          <td>{Number(deets.commNum).toLocaleString()}</td>
+                          <td>{Number(deets.yycNum).toLocaleString()}</td>
+                        </tr>
+                      </React.Fragment>
+                    ))}
+                    <tr>
+                      <td>
+                        <strong>TOTAL</strong>
+                      </td>
+                      <td>
+                        <strong>
+                          {
+                            (activeCrimeStats =
+                              link == "#lst12Mos"
+                                ? Number(
+                                    crimeStats.totalCommCrime12
+                                  ).toLocaleString()
+                                : Number(
+                                    crimeStats.totalCommCrimeMonth
+                                  ).toLocaleString())
+                          }
+                        </strong>
+                      </td>
+                      <td>
+                        <strong>
+                          {
+                            (activeCrimeStats =
+                              link == "#lst12Mos"
+                                ? Number(
+                                    crimeStats.totalYYCCrime12
+                                  ).toLocaleString()
+                                : Number(
+                                    crimeStats.totalYYCCrimeMonth
+                                  ).toLocaleString())
+                          }
+                        </strong>
+                      </td>
+                    </tr>
                   </tbody>
                 </Table>
-            </Card.Body>
-          </Card>
+              </Card.Body>
+            </Card>
           )}
         </Modal.Body>
       </Modal>
