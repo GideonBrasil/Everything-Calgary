7012a107559fc63eec2804764259fbde44c9a7c0
GideonBrasil <gidxx_z@yahoo.com>
2019-04-07 15:55:40 +0000

    multiple markers and infowindows

--- src/GoogleMaps.jsx
+++ src/GoogleMaps.jsx
@@ -18,7 +18,25 @@ class MapContainer extends Component {
   state = {
     showingInfoWindow: true, //Hides or the shows the infoWindow
     activeMarker: {}, //Shows the active marker upon click
-    selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+    selectedPlace: {}, //Shows the infoWindow to the selected place upon a marker
+    markers: [
+      {
+        name: "It's yaaaa Robin",
+        title: "Robin Hood",
+        position: {
+          lat: 51.157707,
+          lng: -114.148879
+        }
+      },
+      {
+        name: "It's yaaaa boi LJ",
+        title: "Little John",
+        position: {
+          lat: 51.156994,
+          lng: -114.151003
+        }
+      }
+    ]
   };
 
   onMarkerClick = (props, marker, e) =>
@@ -41,6 +59,18 @@ class MapContainer extends Component {
     var bounds = new this.props.google.maps.LatLngBounds();
     this.props.polygonCoords.reverse().map(polygon => bounds.extend(polygon));
 
+    let markers = this.state.markers.map(marker => {
+      return (
+        <Marker
+          key={`marker_${marker.name}`}
+          name={marker.name}
+          position={marker.position}
+          title={marker.title}
+          onClick={this.onMarkerClick}
+        />
+      );
+    });
+
     return (
       <Map
         google={this.props.google}
@@ -48,13 +78,22 @@ class MapContainer extends Component {
         initialCenter={this.props.communityCenterLatLong}
         bounds={bounds}
       >
+        {markers}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+        >
+          <div>
+            <h2>{this.state.selectedPlace.name}</h2>
+          </div>
+        </InfoWindow>
         <Polygon
           paths={this.props.polygonCoords}
           strokeColor="#0000FF"
           strokeOpacity={0.8}
           strokeWeight={2}
           fillColor="#0000FF"
-          fillOpacity={0.35}
+          fillOpacity={0.25}
         />
       </Map>
     );
