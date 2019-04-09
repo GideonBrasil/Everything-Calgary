ed7788191e38c9c43cb3154a0787deea94b38fcd
GideonBrasil <gidxx_z@yahoo.com>
2019-04-09 22:58:01 +0000

    fix merge for sidenav

--- src/appComponents/mainComponents/communityComponents/topicComponents/Population.jsx
+++ src/appComponents/mainComponents/communityComponents/topicComponents/Population.jsx
@@ -2,33 +2,6 @@ import React, { Component } from "react";
 import { Modal } from "react-bootstrap";
 import { Line } from "react-chartjs-3";
 
-// const data = {
-//   labels: this.props.data,
-//   datasets: [
-//     {
-//       label: "My First dataset",
-//       fill: false,
-//       lineTension: 0.1,
-//       backgroundColor: "rgba(75,192,192,0.4)",
-//       borderColor: "rgba(75,192,192,1)",
-//       borderCapStyle: "butt",
-//       borderDash: [],
-//       borderDashOffset: 0.0,
-//       borderJoinStyle: "miter",
-//       pointBorderColor: "rgba(75,192,192,1)",
-//       pointBackgroundColor: "#fff",
-//       pointBorderWidth: 1,
-//       pointHoverRadius: 5,
-//       pointHoverBackgroundColor: "rgba(75,192,192,1)",
-//       pointHoverBorderColor: "rgba(220,220,220,1)",
-//       pointHoverBorderWidth: 2,
-//       pointRadius: 1,
-//       pointHitRadius: 10,
-//       data: [65, 59, 80, 81, 56, 55, 40]
-//     }
-//   ]
-// };
-
 class Population extends Component {
   constructor(props) {
     super(props);
