55931fd03add9372fbfd906f9bf96dd74e1188b4
GideonBrasil <gidxx_z@yahoo.com>
2019-04-09 23:42:17 +0000

    clear logs for maps

--- src/appComponents/mainComponents/GoogleMaps.jsx
+++ src/appComponents/mainComponents/GoogleMaps.jsx
@@ -29,7 +29,7 @@ class MapContainer extends Component {
     });
 
   selectIcon = () => {
-    switch(this.props.topic) {
+    switch (this.props.topic) {
       case "construction permits":
         return "../../../public/images/home.png";
       case "traffic incidents":
@@ -39,8 +39,7 @@ class MapContainer extends Component {
       default:
         return "";
     }
-
-  }
+  };
   onClose = props => {
     if (this.state.showingInfoWindow) {
       this.setState({
@@ -53,8 +52,8 @@ class MapContainer extends Component {
   render() {
     var bounds = new this.props.google.maps.LatLngBounds();
     this.props.polygonCoords.reverse().map(polygon => bounds.extend(polygon));
-    const icon = this.selectIcon()
-    // console.log(this.props.pins)
+    const icon = this.selectIcon();
+
     let markers = this.props.pins.map(marker => {
       return (
         <Marker
@@ -64,7 +63,7 @@ class MapContainer extends Component {
           title={marker.address}
           onClick={this.onMarkerClick}
           animation={google.maps.Animation.DROP}
-          icon={ icon }
+          icon={icon}
         />
       );
     });
@@ -76,7 +75,7 @@ class MapContainer extends Component {
           position={marker.location}
           title={marker.address}
           animation={google.maps.Animation.DROP}
-          icon={ "../../../public/images/pedestrian-crossing.png" }
+          icon={"../../../public/images/pedestrian-crossing.png"}
         />
       );
     });
@@ -84,106 +83,112 @@ class MapContainer extends Component {
     const infoData = () => {
       if (this.props.topic === "construction permits") {
         return (
-            <table className="table table-striped">
-              <thead>
-                <tr>
-                  <th scope="col">Permit description:</th>
-                  <th scope="col">
-                    {this.state.selectedPlace.name
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Permit description:</th>
+                <th scope="col">
+                  {this.state.selectedPlace.name
+                    ? this.state.selectedPlace.name.description
                       ? this.state.selectedPlace.name.description
-                        ? this.state.selectedPlace.name.description
-                        : "Unknown"
-                      : ""}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <th scope="row">Project type:</th>
-                  <td>
-                    {this.state.selectedPlace.name
-                      ? this.state.selectedPlace.name.type
-                      : ""}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">Estimated project cost:</th>
-                  <td>
-                    {this.state.selectedPlace.name
-                      ? "$" +
-                          Number(
-                            this.state.selectedPlace.name.estprojectcost
-                          ).toLocaleString() ===
-                        "$NaN"
-                        ? "Project not estimated"
-                        : "$" +
-                          Number(
-                            this.state.selectedPlace.name.estprojectcost
-                          ).toLocaleString()
-                      : ""}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">Project started:</th>
-                  <td>
-                    {this.state.selectedPlace.name
-                      ? this.state.selectedPlace.name.permiteDuration
-                        ? this.state.selectedPlace.name.permiteDuration +
-                          " days ago"
-                        : "Construction project hasn't started"
-                      : ""}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">Permit status:</th>
-                  <td>
-                    {this.state.selectedPlace.name
-                      ? this.state.selectedPlace.name.status
-                      : ""}
-                  </td>
-                </tr>
-                <tr>
-                  <th scope="row">Address:</th>
-                  <td>
-                    {this.state.selectedPlace.name
-                      ? this.state.selectedPlace.name.address
-                      : ""}
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          )} else if (this.props.topic === 'traffic incidents' && this.state.selectedPlace.name) {
-          return (
-            <table className="table table-striped">
-                  <thead>
-                    <tr>
-                      <th scope="col">Traffic Incident:</th>
-                      <th scope="col"> { this.state.selectedPlace.name.address }</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <th scope="row">Time Reported:</th>
-                      <td>
-                        { this.state.selectedPlace.name.timeOf }
-                      </td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Description:</th>
-                      <td>{ this.state.selectedPlace.name.information }</td>
-                    </tr>
-                    <tr>
-                      <th scope="row">Time Cleared:</th>
-                      <td>{ this.state.selectedPlace.name.timeClear }</td>
-                    </tr>
-                  </tbody>
-                </table>
-            )} else if (this.props.topic === 'schools' && this.state.selectedPlace.name){
-                <p> this.state.selectedPlace.name.address </p>
-          } else {
-              return ""
-            }
-          }
+                      : "Unknown"
+                    : ""}
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Project type:</th>
+                <td>
+                  {this.state.selectedPlace.name
+                    ? this.state.selectedPlace.name.type
+                    : ""}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Estimated project cost:</th>
+                <td>
+                  {this.state.selectedPlace.name
+                    ? "$" +
+                        Number(
+                          this.state.selectedPlace.name.estprojectcost
+                        ).toLocaleString() ===
+                      "$NaN"
+                      ? "Project not estimated"
+                      : "$" +
+                        Number(
+                          this.state.selectedPlace.name.estprojectcost
+                        ).toLocaleString()
+                    : ""}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Project started:</th>
+                <td>
+                  {this.state.selectedPlace.name
+                    ? this.state.selectedPlace.name.permiteDuration
+                      ? this.state.selectedPlace.name.permiteDuration +
+                        " days ago"
+                      : "Construction project hasn't started"
+                    : ""}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Permit status:</th>
+                <td>
+                  {this.state.selectedPlace.name
+                    ? this.state.selectedPlace.name.status
+                    : ""}
+                </td>
+              </tr>
+              <tr>
+                <th scope="row">Address:</th>
+                <td>
+                  {this.state.selectedPlace.name
+                    ? this.state.selectedPlace.name.address
+                    : ""}
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      } else if (
+        this.props.topic === "traffic incidents" &&
+        this.state.selectedPlace.name
+      ) {
+        return (
+          <table className="table table-striped">
+            <thead>
+              <tr>
+                <th scope="col">Traffic Incident:</th>
+                <th scope="col"> {this.state.selectedPlace.name.address}</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">Time Reported:</th>
+                <td>{this.state.selectedPlace.name.timeOf}</td>
+              </tr>
+              <tr>
+                <th scope="row">Description:</th>
+                <td>{this.state.selectedPlace.name.information}</td>
+              </tr>
+              <tr>
+                <th scope="row">Time Cleared:</th>
+                <td>{this.state.selectedPlace.name.timeClear}</td>
+              </tr>
+            </tbody>
+          </table>
+        );
+      } else if (
+        this.props.topic === "schools" &&
+        this.state.selectedPlace.name
+      ) {
+        <p> this.state.selectedPlace.name.address </p>;
+      } else {
+        return "";
+      }
+    };
 
     return (
       <Map
@@ -198,12 +203,11 @@ class MapContainer extends Component {
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
-          >
+        >
           <div>{infoData()}</div>
-
         </InfoWindow>
 
-          {signalMarkers}
+        {signalMarkers}
         <Polygon
           paths={this.props.polygonCoords}
           strokeColor="#0000FF"
