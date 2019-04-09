7c4f1c8ed4e3205dbe821d06877813bfb8b29a69
GideonBrasil <gidxx_z@yahoo.com>
2019-04-09 16:34:43 +0000

    traffic fix

--- src/appComponents/mainComponents/calgaryComponents/topicComponents/TrafficIncidents.jsx
+++ src/appComponents/mainComponents/calgaryComponents/topicComponents/TrafficIncidents.jsx
@@ -9,7 +9,6 @@ class TrafficIncidents extends Component {
     };
   }
   componentDidMount() {
-
     fetch(`http://localhost:3000/traffic`)
       .then(res => res.json())
       .then(data => {
@@ -22,71 +21,78 @@ class TrafficIncidents extends Component {
       });
   }
 
-
   render() {
     return (
-      <Modal id="traffic-modal"
-      show
-      onHide={() => this.props.changeTopic("")}
-      size="lg"
-      aria-labelledby="contained-modal-title-vcenter"
-      centered
-      dialogClassName="modal-60w"
+      <Modal
+        id="traffic-modal"
+        show
+        onHide={() => this.props.changeTopic("")}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+        dialogClassName="modal-60w"
       >
-      <Modal.Header closeButton>
-        <Modal.Title>Current Traffic Incidents</Modal.Title>
-      </Modal.Header>
-  <Modal.Body className="traffic-body">
-    {!this.state.traffic ? <h4>Loading...</h4> : (
-    <Card>
-      {this.state.traffic.description === 'No current incident' ? 
-        <React.Fragment>
-          <Card.Header>
-            <Nav>
-              <Nav.Item>
-                <h4>No current incidents, the roads are clear!</h4>
-              </Nav.Item>
-            </Nav>
-          </Card.Header>
-        </React.Fragment> : (
-          <React.Fragment>
-            <Card.Header>
-              <Nav>
-                <Nav.Item>
-                  <h4>Right now on the road...</h4>
-                </Nav.Item>
-              </Nav>
-            </Card.Header>
-            <Card.Body className="traffic-card">
-                  <Table striped bordered hover>
-                    <thead>
-                      <tr>
-                        <th>Date</th>
-                        <th>Start Time</th>
-                        <th>Info</th>
-                        <th>Description</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {this.state.traffic.map(incident => (
-                        <React.Fragment key={incident.start_dt}>
-                          <tr>
-                            <td className="date">{incident.start_dt.substring(0, 10)}</td>
-                            <td className="date">{incident.start_dt.substring(11, 19)}</td>
-                            <td>{incident.incident_info}</td>
-                            <td>{incident.description}</td>
-                          </tr>
-                        </React.Fragment> 
-                      ))}
-                    </tbody>
-                  </Table>
-              </Card.Body>
-          </React.Fragment>
-        )}
-      </Card>
-    )}
-    </Modal.Body>
-  </Modal>
+        <Modal.Header closeButton>
+          <Modal.Title>Current Traffic Incidents</Modal.Title>
+        </Modal.Header>
+        <Modal.Body className="traffic-body">
+          {!this.state.traffic ? (
+            <h4>Loading...</h4>
+          ) : (
+            <Card>
+              {this.state.traffic.description === "No current incident" ? (
+                <React.Fragment>
+                  <Card.Header>
+                    <Nav>
+                      <Nav.Item>
+                        <h4>No current incidents, the roads are clear!</h4>
+                      </Nav.Item>
+                    </Nav>
+                  </Card.Header>
+                </React.Fragment>
+              ) : (
+                <React.Fragment>
+                  <Card.Header>
+                    <Nav>
+                      <Nav.Item>
+                        <h4>Right now on the road...</h4>
+                      </Nav.Item>
+                    </Nav>
+                  </Card.Header>
+                  <Card.Body className="traffic-card">
+                    <Table striped bordered hover>
+                      <thead>
+                        <tr>
+                          <th>Date</th>
+                          <th>Start Time</th>
+                          <th>Info</th>
+                          <th>Description</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {this.state.traffic.map(incident => (
+                          <React.Fragment key={incident.start_dt}>
+                            <tr>
+                              <td className="date">
+                                {incident.start_dt.substring(0, 10)}
+                              </td>
+                              <td className="date">
+                                {incident.start_dt.substring(11, 19)}
+                              </td>
+                              <td>{incident.incident_info}</td>
+                              <td>{incident.description}</td>
+                            </tr>
+                          </React.Fragment>
+                        ))}
+                      </tbody>
+                    </Table>
+                  </Card.Body>
+                </React.Fragment>
+              )}
+            </Card>
+          )}
+        </Modal.Body>
+      </Modal>
     );
   }
 }
